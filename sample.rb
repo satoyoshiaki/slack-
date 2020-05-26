e72ea4c4ae64fcc493c03a9b0a57d27102398909
佐藤良昭 <yoshiprograming@gmail.com>
2020-05-26 15:36:59 +0900

    スラックぼっと完成

---
+++ sample.rb
@@ -0,0 +1,12 @@
+require "slack-notify"
+require 'clockwork'
+require 'active_support/time'
+require 'dotenv/load'
+
+include Clockwork
+
+
+sample = SlackNotify::Client.new(webhook_url: ENV['URL'])
+every(10.minutes, 'bot1') do
+    sample.notify("スラックぼっと")
+end
